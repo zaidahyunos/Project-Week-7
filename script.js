d54dea427d7bdd9139d8baab5528fcd0b5540247
zaidahyunos <zaidah.yunos@gmail.com>
2024-01-17 15:00:51 +0800

    update script.js

--- script.js
+++ script.js
@@ -17,37 +17,7 @@ function updateWeatherInfo(response) {
 }
 
 function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  let day = days[date.getDay()];
-  let month = months[date.getMonth()];
-  let year = date.getFullYear();
-
-  return `${day} ${date} ${month} ${year} ${hours}:${minutes}`;
+  return `${date}`;
 }
 
 function searchCity(city) {
